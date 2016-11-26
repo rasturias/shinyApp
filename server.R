eb47d49a6707c46a4b9282ab416010be648755e5
Ramiro Asturias <ramiro_asturias@hotmail.com>
2016-11-26 15:18:22 -0600

    Shiny App Created

--- server.R
+++ server.R
@@ -1,23 +1,53 @@
 
 # This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
 
 library(shiny)
+library(UsingR)
+library(ggplot2)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$dimondDataPlot <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+        
+          #First, let’s fit the linear regression model. 
+          fit <- lm(price*0.7 ~ carat, data = diamond)
+          
+          
+          #Now, let’s predicting the price of a diamond. 
+          newx <- c(input$carats)
+          pricePredicted <- predict(fit, newdata = data.frame(carat = newx))
+                    
+          
+    #Lets Plot data
+    data(diamond)
+    g = ggplot(diamond, aes(x = carat, y = price*0.7))
+    g = g + xlab("Mass (carats)")
+    g = g + ylab("Price (USD $)")
+    g = g + geom_point(size = 3, colour = "black", alpha=0.5)
+    g = g + geom_point(size = 5, colour = "blue", alpha=0.2)
+    g = g + geom_smooth(method = "lm", colour = "black")
+    g = g + geom_vline(xintercept = input$carats, label("C") )
+    g = g + geom_hline(yintercept = pricePredicted[1] )
+    g          
+          
   })
 
+
+  output$pricePrediction <- renderText({
+
+          #First, let’s fit the linear regression model. 
+          fit <- lm(price*0.7 ~ I(carat * 10), data = diamond)
+          
+          
+          #Now, let’s predicting the price of a diamond. 
+          newx <- c(input$carats)
+          pricePredicted <- predict(fit, newdata = data.frame(carat = newx))
+          pricePredicted[1]
+  })
+  
+  
+  
+  
+  
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,37 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# Based on diamond data set from the UsingR package. 
+# The data is diamond prices (in Singapore dollars) and diamond weight in carats. 
+# Carats are a standard measure of diamond mass, 0.2 grams. 
+# We will predict the price for the diamond based on the Carat Mass
 
 library(shiny)
 
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Predict Diamonds Price based On Weight"),
 
-  # Sidebar with a slider input for number of bins
+  # Sidebar with a slider input for Carats
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      h2("Instructions"),
+      p("Using a diamond data which contains Price based on Carat. A Linear model is created
+         to predict any price for a given diamond mass in carats.
+         Diamond prices  are in USD dollars and diamond weight in carats. 
+         Carats are a standard measure of diamond mass, 0.2 grams."),
+      sliderInput("carats",
+                  "Select Diamond MASS in Carats:",
+                  min = 0.10,
+                  max = 1.5,
+                  value = 0.15)
     ),
 
-    # Show a plot of the generated distribution
+    # Show a plot with the data for the prediction model
     mainPanel(
-      plotOutput("distPlot")
+      h3("Price in USD Calculated "),
+      h2(textOutput("pricePrediction")),
+      plotOutput("dimondDataPlot")
+      
     )
   )
 ))
